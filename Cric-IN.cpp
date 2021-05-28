d97dcb228e86027684e5f048a94180bf9536df2e
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-05-28 19:43:07 +0530

    Update Cric-IN.cpp

--- Cric-IN.cpp
+++ Cric-IN.cpp
@@ -1,11 +1,22 @@
+//custom header file to develop this game
 #include "GameHeader.h"
+
 #include <iostream>
 using namespace std;
 
 int main(){
+    // pool of players
     string pool[] = { "Virat","Rohit","Dhawan","Pant","Karthik","Rahul","Jadeja","Hardik","Bumrah","BKumar","Ishant" };
+    
+    //create game object 
     Game cricket;
+    
+    //set the the player array
     cricket.setPlayers(11,pool);
+    
+    // game begins: welcome message, creating team, toss and initialize first player of the the First inning
     cricket.welcomeScreen();
+    
+    //playing the game
     cricket.playingGame();
 }
