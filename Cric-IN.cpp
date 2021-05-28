c143d17c49aefcf5082648019b289539eb1b5f79
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-05-28 12:04:05 +0530

    Update Cric-IN.cpp

--- Cric-IN.cpp
+++ Cric-IN.cpp
@@ -3,19 +3,10 @@
 using namespace std;
 
 int main(){
-    string pool[] = { "Virat","Rohit","Dhawan","Pant","Karthik","Rahul","Jadeja","Hardik","Bumrah","BKumar","Ishant" };
+   string pool[] = { "Virat","Rohit","Dhawan","Pant","Karthik","Rahul","Jadeja","Hardik","Bumrah","BKumar","Ishant" };
     Game cricket;
     cricket.setPlayers(11,pool);
     cricket.welcomeScreen();
-    //cricket.assignPlayersToTeams();
-    //cricket.performToss();
-
-    //string team1[] = { "Virat","Rohit","Dhawan","Pant","Karthik" };
-    //string team2[] = { "Rahul","Jadeja","Hardik","Bumrah","BKumar" };
-    
     cricket.gamePlay(1);
     cricket.playingGame();
-    //cricket.finalScoreCard();
-    /*int runs = cricket.getRunsScored();
-    cout << "\n Runs " << runs;*/
 }
