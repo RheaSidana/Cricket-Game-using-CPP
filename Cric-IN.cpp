c6724f1832be2aa72a6371e4e40794d7bb5686d4
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-05-28 13:12:42 +0530

    Update Cric-IN.cpp

--- Cric-IN.cpp
+++ Cric-IN.cpp
@@ -7,20 +7,5 @@ int main(){
     Game cricket;
     cricket.setPlayers(11,pool);
     cricket.welcomeScreen();
-    //cricket.assignPlayersToTeams();
-    //cricket.performToss();
-
-    /*string team1[] = { "Virat","Rohit","Dhawan","Pant","Karthik" };
-    string team2[] = { "Rahul","Jadeja","Hardik","Bumrah","BKumar" };*/
-    
-    //cricket.gamePlay(1);
-    //cricket.playingGame();
-    /*Team teamA;
-    teamA.assignPlayers(5,team1);
-    Team teamB;
-    teamB.assignPlayers(5, team2);*/
-    //Game cricket;
-    //cricket.finalScoreCard();
-    /*int runs = cricket.getRunsScored();
-    cout << "\n Runs " << runs;*/
+   
 }
