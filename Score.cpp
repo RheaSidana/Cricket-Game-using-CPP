bd90d0d85329146ce9f9b8ce4c60b64b63562f4d
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2021-05-31 18:57:18 +0530

    Update Score.cpp

--- Score.cpp
+++ Score.cpp
@@ -3,47 +3,58 @@
 #include <string>
 using namespace std;
 
+//returns the count of ball of the batting player
 int Score :: getBatBalls() {
 	return batBalls;
 }
 
+//initialise the data members 
 void Score::init() {
 	runs = batBalls = bowlBalls = wickets = 0;
 	overs = 0;
 }
 
+//set the count of balls of the batting player 
 void Score::setBatBalls(int ball) {
 	batBalls = ball;
 }
 
+//set the count of balls of the bowling player 
 void Score::setBowlBalls(int ball) {
 	bowlBalls = ball;
 }
 
+//return the count of balls of the bowling player 
 int Score::getBowlBalls() {
 	return bowlBalls;
 }
 
+//return count of runs of the batting player 
 int  Score::getRuns() {
 	return runs;
 }
 
+//set count of runs of the batting player 
 void Score::setRuns(int run) {
 	runs = run;
 }
 
+//return count of wicket of the bowling player 
 int Score:: getWickets() {
 	return wickets;
 }
 
+//return count of wicket of the bowling player 
 void Score::setWickets(int wicket) {
 	wickets = wicket;
 }
 
+//return count of overs of the bowling player 
 float Score::getOvers() {
 	return overs;
 }
 
+//return count of overs of the bowling player 
 void Score::setOvers(float over) {
 	overs = over;
 }
